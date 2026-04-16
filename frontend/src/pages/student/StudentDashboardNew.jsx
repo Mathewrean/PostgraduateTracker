e8f34aa29e3bd584059bcaae37beb90ac9f5a6ab
Mathewrean <mathewrean@gmail.com>
2026-04-16 21:25:28 +0300

    remove test mds

--- frontend/src/pages/student/StudentDashboardNew.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Layout } from '../../components/Layout';
-import { Card, StatCard } from '../../components/Card';
-import { Carousel } from '../../components/Carousel';
-import
