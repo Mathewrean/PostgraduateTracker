f8d3c3e1f6f24992514cbb6369a2043c3bce7263
Mathewrean <mathewrean@gmail.com>
2026-07-22 11:07:59 +0300

    Fix reset page crash and users migration drift

--- frontend/src/pages/auth/ResetPasswordPage.jsx
+++ frontend/src/pages/auth/ResetPasswordPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate, useLocation, Link } from 'react-router-dom'
 import { authService } from '../../services'
 import { TopbarBrand } from '../../components/TopbarBrand'
 import toast from 'react-hot-toast'
